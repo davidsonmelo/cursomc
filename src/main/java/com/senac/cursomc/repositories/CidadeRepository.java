952a0ed25f1e3fc3422d352c4320c25178feda4b
davidsonmelo <davidsonpedrozamelo@gmail.com>
2022-02-28 15:53:59 -0300

    version_0.9 Estado e Cidade

---
+++ src/main/java/com/senac/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.senac.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.senac.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+	
+}
---
+++ src/main/java/com/senac/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.senac.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.senac.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+	
+}
